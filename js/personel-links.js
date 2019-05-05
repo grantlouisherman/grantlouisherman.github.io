82109b1ad17b53f0854d5ff4993d4687384d5a74
Grant Herman <grantlouisherman041@gmail.com>
2019-05-04 17:40:33 -0700

    adding resume

--- js/personel-links.js
+++ js/personel-links.js
@@ -1,37 +1,46 @@
 
-const personelLinksLayout = {
-  "github": {
+const personelLinksLayout = [
+  {
+    "key": "github",
     "link":"https://github.com/grantlouisherman",
     "icon":"./images/github.png"
   },
-  "linkedin": {
+  {
+    "key": "linkedin",
     "link":"https://www.linkedin.com/in/grant-herman-3b34258a/",
     "icon": "./images/linkedin.png"
   },
-  "stackoverflow": {
+  {
+    "key": "stackoverflow",
     "link": "https://stackoverflow.com/users/4732217/grant-herman",
     "icon": "./images/stackoverflow.png"
+  },
+  {
+    "key": "resume",
+    "link":"./additionalContent/resume.pdf",
+    "icon": "./images/resume.png",
+    "download":true
   }
-}
+]
 
-const personalLinkComponent = (imgUrl, srcLink, linkName) => {
+const personalLinkComponent = (key, link, icon, download) => {
   const container = document.createElement('div')
-  const icon = new Image();
-  icon.src = imgUrl
-  icon.class = 'linkIcon'
-  const link = document.createElement('a')
-  link.setAttribute('href', srcLink)
-  link.append(icon)
-  container.append(link)
+  container.setAttribute('class', 'column')
+  const linkIcon = new Image()
+  linkIcon.src = icon
+  linkIcon.class = 'linkIcon'
+  const externalLink = document.createElement('a')
+  externalLink.setAttribute('href', link)
+  externalLink.append(linkIcon)
+  container.append(externalLink)
   return container
 }
 
 const personalLinkComponentCreator = () => {
-  const linksKeys = Object.keys(personelLinksLayout)
   const personelLinksContainer = document.getElementById('personelLinksContainer')
-  linksKeys.forEach(linkKey => {
-    const { link, icon } = personelLinksLayout[linkKey]
-    personelLinksContainer.append(personalLinkComponent(icon, link, linkKey))
+  personelLinksLayout.forEach(links => {
+    const { key, link, icon } = links
+    personelLinksContainer.append(personalLinkComponent(key, link, icon, links.download))
   })
   return personelLinksContainer
 }
